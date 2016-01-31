f6bd71fa9875f617116bb72609745ae50ff97aad
Lee Xiang Wei <xwlee2009@gmail.com>
2016-01-31 13:15:52 +0800

    Complete project setup

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Places
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'pp'
+Photo.all.each { |photo| photo.destroy }
+Place.all.each { |place| place.destroy }
+Place.create_indexes
+Place.load_all(File.open('./db/places.json'))
+Dir.glob("./db/image*.jpg") {|f| photo=Photo.new; photo.contents=File.open(f,'rb'); photo.save}
+Photo.all.each {|photo| place_id=photo.find_nearest_place_id 1*1609.34; photo.place=place_id; photo.save}
+pp Place.all.reject {|pl| pl.photos.empty?}.map {|pl| pl.formatted_address}.sort
